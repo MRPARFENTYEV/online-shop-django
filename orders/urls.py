0d6e20f84bf88729f3306261a5f44f13e1772ee5
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2024-04-05 19:30:44 +0300

    change status is working at all and it is possible to create order

--- orders/urls.py
+++ orders/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('list', views.user_orders, name='user_orders'),
     path('checkout/<int:order_id>', views.checkout, name='checkout'),
     path('fake-payment/<int:order_id>', views.fake_payment, name='pay_order'),
+    path('no_way_to_order', views.no_way_to_order, name='no_way_to_order')
     # path('not_enough_quantity',views.not_enough_quantity, name='not_enough_quantity')
 ]
--- orders/views.py
+++ orders/views.py
@@ -13,27 +13,67 @@ def not_enough_quantity(request):
     context = {'Not_enough': 'Товара недостаточно'}
     print('not_enough_quantity',request)
     return render(request, 'not_enough_quantity.html', context)
+
+def no_way_to_order(request):
+    context={'order':'order'}
+    return render(request,'no_way_to_order.html',context)
+
 @login_required
 def create_order(request):
     cart = Cart(request)
     order = Order.objects.create(user=request.user)
-    # product_quantity = OrderItem.objects
-
+    products = Product.objects.filter(avaliable=False)
+    for product in products:
+        product_slug = product.slug
+        print(product_slug)
     for item in cart:
         product_get = Product.objects.get(slug=item['product'])
         product_get_quantity = product_get.quantity
-        if item['quantity'] > product_get_quantity:
-            return not_enough_quantity(request)
-    else:
-        OrderItem.objects.create(
-            order=order, product=item['product'],
-            price=item['price'], quantity=item['quantity']
-    )
-        product_get.quantity = product_get.quantity - item['quantity']
-        product_get.save()
+        # print(product_get)
+        # print(str(product_slug)==str(product_get))
+        if str(product_slug)==str(product_get):
+            return redirect('orders:no_way_to_order')
+        else:
+            # product_get = Product.objects.get(slug=item['product'])
+            # product_get_quantity = product_get.quantity
 
-        # .update(quantity=Product.quantity - item['quantity'])
-    return redirect('orders:pay_order', order_id=order.id)
+            if item['quantity'] > product_get_quantity:
+                return not_enough_quantity(request)
+            else:
+                OrderItem.objects.create(
+                    order=order, product=item['product'],
+                    price=item['price'], quantity=item['quantity']
+            )
+            product_get.quantity = product_get.quantity - item['quantity']
+            product_get.save()
+
+                # .update(quantity=Product.quantity - item['quantity'])
+            return redirect('orders:pay_order', order_id=order.id)
+
+
+
+    # product_quantity = OrderItem.objects
+    # for product in products:
+
+    #         for item in cart:
+    #             print(item)
+    #             product_get = Product.objects.get(slug=item['product'])
+    #             product_get_quantity = product_get.quantity
+    #
+    #             if item['quantity'] > product_get_quantity:
+    #                 return not_enough_quantity(request)
+    #             else:
+    #                 OrderItem.objects.create(
+    #                     order=order, product=item['product'],
+    #                     price=item['price'], quantity=item['quantity']
+    #             )
+    #             product_get.quantity = product_get.quantity - item['quantity']
+    #             product_get.save()
+    #
+    #                 # .update(quantity=Product.quantity - item['quantity'])
+    #             return redirect('orders:pay_order', order_id=order.id)
+    #     else:
+
 
 
 @login_required
